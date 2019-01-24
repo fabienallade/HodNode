1d12b314dfccce1aa7008b2e5c303a5dc79f6dc0
Allade fabien <alladefabien@gmail.com>
2019-01-24 12:49:45 +0100

    premier commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const config = require('config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const express = require('express');
+
+if (!config.get('jwtPrivateKey')) {
+  console.log('Fatal Error:la config ');
+  process.exit(1);
+}
+
+const app = express();
+app.use(morgan('tiny'));
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
