4f223a8bea63ee32cae6a2520abdfec406126b61
Allade fabien <alladefabien@gmail.com>
2019-03-21 12:06:17 +0100

    heroku go

---
+++ README.md
@@ -0,0 +1,1 @@
+ici le code ne sert qua faire des test pout le moment on va bien faire la prochaine fois
--- index.js
+++ index.js
@@ -30,5 +30,5 @@ app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
