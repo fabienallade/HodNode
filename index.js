bb51ae8afa97f74e488f87c4ff7f6bbcd64866df
Allade fabien <alladefabien@gmail.com>
2019-07-10 17:50:13 +0100

    ah node js inh

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ if (!config.get('jwtPrivateKey')) {
 
 const app = express();
 app.use(morgan('tiny'));
-const db = config.get('db')
+const db = config.get('db');
+if (!config.get('db')) {
+  console.log("erreur sur les chose");
+}
 mongoose.connect(db)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
